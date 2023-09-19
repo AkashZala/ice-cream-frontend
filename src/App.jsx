7cbc82a3085509d00237d668019785d170a7507c
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-09-19 16:24:05 -0400

    display flavors, added delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, useLocation } from 'react-router-dom';
 
 function App() {
   const [flavors, setFlavors] = useState([]);
+  const location = useLocation().pathname;
 
   useEffect(() => {
     const fetchFlavors = async () => {
@@ -12,6 +14,14 @@ function App() {
     fetchFlavors();
   }, []);
 
+  
+  const flavor = flavors.find(flavor => flavor.id === location.slice(13,location.length)*1);
+
+  const deleteFlavor = async (flavor) => {
+    const response = await axios.delete(`http://localhost:3000/api/flavors/${flavor.id}`);
+    setFlavors(flavors.filter(_flavor => _flavor.id !== flavor.id));
+  }
+
   return (
     <>
       <h1>Ice Cream Flavors ({flavors.length})</h1>
@@ -20,12 +30,18 @@ function App() {
           flavors.map(flavor => {
             return (
               <li key={flavor.id}>
-                {flavor.favorite ? `${flavor.name} *` : flavor.name}
+                <Link to={`/api/flavors/${flavor.id}`}>
+                  {flavor.favorite ? `* ${flavor.name}` : flavor.name}
+                </Link>
+                <button onClick={() => {deleteFlavor(flavor)}}>
+                  X
+                </button>
               </li>
             );
           })
         }
       </ul>
+      <p>* Favorite</p>
     </>
   )
 }
